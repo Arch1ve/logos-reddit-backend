5e264fef5f378b74f9ca84bb7a8b56d5f0dd7d9d
Arch1ve <afanasiy.m2002@gmail.com>
2025-05-26 14:53:42 +0300

    create user model

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,29 @@
 import express, { Request, Response } from 'express';
+import * as mongoose from "mongoose";
+import User from "./models/user";
 
-// mongoose.connect(DATABASE_URL || '');
+mongoose.connect("mongodb://localhost:27017/logos");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Работает")
 })
+
+// app.post("/user/create", (req: Request, res: Response) => {
+//   const {email, password, username} = req.body
+//
+//   User.create({ email, password, username })
+//     .then((user) => {
+//       res.send(user)
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(403).send()
+//     })
+// })
 
 app.use((req: Request, res: Response) => res.status(404).send("Страница не найдена"));
 
---
+++ src/models/user.ts
@@ -0,0 +1,30 @@
+import {model, Schema} from "mongoose";
+import isEmail from "validator/lib/isEmail";
+
+interface User {
+  password: string;
+  email: string;
+  username: string;
+}
+
+const UserSchema = new Schema<User>({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [isEmail]
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 4,
+    maxlength: 20
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+})
+
+export default model<User>("user", UserSchema);
